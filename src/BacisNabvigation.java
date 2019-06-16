302c23b11e5382218b21d80ebab8d1ad7b72db25
abida927 <abide0927@gmail.com>
2019-06-16 14:03:58 -0700

    some changes...

--- src/BacisNabvigation.java
+++ src/BacisNabvigation.java
@@ -3,5 +3,6 @@ public class BacisNabvigation {
     public static void main(String[] args) {
 
         System.out.println("let's try git");
+        System.out.println("some changes...");
     }
 }
